856cc9e1309f1ec80f923313445095a92e923eab
hamid1103 <35217917+hamid1103@users.noreply.github.com>
2023-05-18 13:39:15 +0200

    Added Joystick library

--- src/game.js
+++ src/game.js
@@ -3,13 +3,17 @@ import {ResourceLoader} from "./resources.js";
 import {variants, labels} from '@catppuccin/palette'
 import {Physics} from "excalibur";
 import {TestLevel} from "./Levels/TestLevel.js";
+import { Arcade } from "arcade-game"
 import {DevTool} from "@excaliburjs/dev-tools"; //Using catppuccin colors found in this package. Check here for more info https://github.com/catppuccin/catppuccin
 
 const loader = ResourceLoader
 export class Game extends ex.Engine {
+    #arcade;
+    #joystickListener;
     constructor() {
         super({
-            displayMode: ex.DisplayMode.FitScreenAndFill,
+            width: 1440,
+            height: 900,
             antialiasing: false,
             fixedUpdateFps: 60,
             //suppressPlayButton: true,
@@ -24,9 +28,29 @@ export class Game extends ex.Engine {
     }
 
     startGame(){
+        this.#arcade = new Arcade(this, false, true)
+        this.#joystickListener = (e) => this.#joyStickFound(e)
+        document.addEventListener("joystickcreated",  this.#joystickListener)
         const testLevel = new TestLevel()
         this.addScene('TL', testLevel)
         this.goToScene('TL')
+    }
+
+    #joyStickFound(e) {
+        let joystick = this.#arcade.Joysticks[e.detail]
+
+        // debug, this shows you the names of the buttons when they are pressed
+        for (const buttonEvent of joystick.ButtonEvents) {
+            document.addEventListener(buttonEvent, () => console.log(buttonEvent))
+        }
+
+        this.update();
+    }
+
+    onPreUpdate(_engine, _delta) {
+        for (let joystick of this.#arcade.Joysticks) {
+            joystick.update()
+        }
     }
 
 }
