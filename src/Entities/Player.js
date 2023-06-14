10971116cdbdd5eb4f140f6f9d355b4878722181
hamid1103 <35217917+hamid1103@users.noreply.github.com>
2023-06-14 13:02:50 +0200

    Added basic player movement

--- src/Entities/Player.js
+++ src/Entities/Player.js
@@ -1,6 +1,8 @@
 import * as ex from 'excalibur'
 import {Resources} from "../resources.js";
+import {Shape, Sprite} from "excalibur";
 export class Player extends ex.Actor{
+    onGround;
     startpos;
     /**
      *
@@ -15,17 +17,48 @@ export class Player extends ex.Actor{
         });
         this.startpos = position
     }
-    Target = ex.Sprite.from(Resources.TargetIcon)
+
+    Target = Sprite.from(Resources.TargetIcon)
 
     onInitialize(_engine) {
         this.graphics.add('Sprite', this.Target)
         this.on('exitviewport', ()=>{
             this.pos = this.startpos;
         })
+        this.on('collisionstart', (e)=>this.onFirstCollision)
+        this.on('postcollision', e=>this.postCollision())
     }
+
+    onFirstCollision(e){
+
+    }
+
+    postCollision(e){
+        if(e.other.side === ex.Side.Top){
+            this.onGround = true
+        }
+    }
+
     onPreUpdate(_engine, _delta) {
         this.graphics.use('Sprite')
-        this.acc = ex.vec(0, 10)
+
+        if(_engine.input.keyboard.isHeld(ex.Input.Keys.Right)){
+            this.vel.x = 200
+        }
+
+        if(_engine.input.keyboard.isHeld(ex.Input.Keys.Left)){
+            this.vel.x = -200
+        }
+
+        if(_engine.input.keyboard.isHeld(ex.Input.Keys.Left) && this.onGround) {
+            this.vel.y = -400;
+            this.onGround = false;
+        }
+
+        if(this.vel.y > 0 || this.vel.y < 0){
+            this.onGround = false
+        }
+
     }
 
 }
