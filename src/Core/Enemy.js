2bf539434c70b07229eb62b8c1bfa48cb459bee8
Atal09 <113996359+Atal09@users.noreply.github.com>
2023-06-15 11:08:45 +0200

    update.enemy.js
    even uittesten

--- src/Core/Enemy.js
+++ src/Core/Enemy.js
@@ -1,6 +1,7 @@
 import * as ex from 'excalibur'
 import { CollisionType, Sprite, Vector} from 'excalibur';
 import {Resources, SpriteResources} from "../resources.js";
+import { Player } from '../Entities/Player.js';
 
 
 
@@ -9,15 +10,33 @@ import {Resources, SpriteResources} from "../resources.js";
 
 
 export class Enemy extends ex.Actor{
+    Kwal = ex.Sprite.from(Resources.Enemy1)
+    Duif = ex.Sprite.from(Resources.Enemy2)
+    
+
     constructor(){
         super({
+            // collisionType: CollisionType.Active
+            
 
-
-        
-        
         });
 
 
     }
+
+
+    // onInitialize() {
+    //     // this.on('precollision', this.handleCollision);
+    //   }
+    
+    //   handleCollision(event) {
+    //     if (event.other instanceof Player) {
+    //       event.other.takeDamage();
+    //     }
+    //   }
+
+
+
+
 
 }
