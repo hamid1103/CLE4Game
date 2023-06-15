5cf1b0c7c8c7bee0cca6226a05bf3ea8f3aa5c4b
Atal09 <113996359+Atal09@users.noreply.github.com>
2023-06-15 12:01:29 +0200

    scale
    scale emey

--- src/Core/Enemy.js
+++ src/Core/Enemy.js
@@ -11,38 +11,40 @@ import {TestLevel} from '../Levels/TestLevel.js';
 
 
 export class Enemy extends ex.Actor{
-    // Kwal = ex.Sprite.from(Resources.Enemy1)
-    // Duif = ex.Sprite.from(Resources.Enemy2)
+ 
 
    
 
-    // constructor(){
-    //     super({
-    //         // collisionType: CollisionType.Active
+    constructor(){
+        super({
+            collisionType: CollisionType.Active
             
 
-    //     });
+        });
 
 
-    // }
+    }
 
 
     onInitialize() {
-        // this.on('precollision', this.handleCollision);
+        this.on('precollision', this.handleCollision);
        const Kwal = ex.Sprite.from(Resources.Enemy1)
+       Kwal.scale.setTo(2, 2);
+
+
        const Duif = ex.Sprite.from(Resources.Enemy2)
+       Duif.scale.setTo(1.5, 1.5);
+
+
+
       }
 
 
     
-      handleCollision(event) {
-        if (event.other instanceof Player) {
-          event.other.takeDamage();
-        }
-      }
-
-
-
-
+    // handleCollision(event) {
+    //     if (event.other instanceof Player) {
+    //         event.other.RemoveHeart();
+    //     }
+    // }
 
 }
