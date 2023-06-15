dfef6a7860693e399715ea6dad5b82ca9d41a91c
RaymondLam18 <1024322@hr.nl>
2023-06-15 10:46:46 +0200

    Update resources.js

--- src/resources.js
+++ src/resources.js
@@ -1,13 +1,25 @@
 import { ImageSource, Sound, Resource, Loader, Sprite } from "excalibur";
 import TargetIcon from './assets/finishedAssets/Target.png'
 import pixelplatform from './assets/finishedAssets/platform.png'
-
-
+import background from './assets/finishedAssets/background_start.png'
+import duif from './assets/finishedAssets/duif.png'
+import kwal from './assets/finishedAssets/jellyfish.png'
+import jetpack from './assets/finishedAssets/jetpack.png'
+import nuclear from './assets/finishedAssets/nuclear.png'
+import rocket from './assets/finishedAssets/rocket.png'
+import star from './assets/finishedAssets/star.png'
 
 
 const Resources = {
     TargetIcon: new ImageSource(TargetIcon),
     Pixelplatform: new ImageSource(pixelplatform),
+    Background: new ImageSource(background),
+    Enemy1: new ImageSource(kwal),
+    Enemy2: new ImageSource(duif),
+    Jetpack: new ImageSource(jetpack),
+    Nuclear: new ImageSource(nuclear),
+    Rocket: new ImageSource(rocket),
+    Star: new ImageSource(star),
 }
 
 const SpriteResources = {
