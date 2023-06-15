a1a85dc7995beaa0bb1d4bdf2b484c5fe090ff01
Ricky <45554458+ikeetkip@users.noreply.github.com>
2023-06-15 13:31:18 +0200

    updated testlevel
    made a nested loop

--- src/Levels/TestLevel.js
+++ src/Levels/TestLevel.js
@@ -1,31 +1,22 @@
 import * as ex from 'excalibur'
-import {Player, PlayerName} from "../Entities/Player.js";
+import {Player} from "../Entities/Player.js";
 import {Platform} from "../Core/Platform.js";
 import {Enemy} from "../Core/Enemy.js";
 import {ScrollingBackground} from "../Core/Area.js";
 
 
 export class TestLevel extends ex.Scene{
-    player1;
-    player2;
-
     onInitialize(_engine) {
         this.StartLevel()
     }
 
 
     StartLevel(){
-        this.player1 = new Player(650, 700, PlayerName.Player1)
-        this.add(this.player1)
-
-        this.player2 = new Player(850, 700, PlayerName.Player2)
-        this.add(this.player2)
+        let player = new Player(650, 700)
+        this.add(player)
 
         let platform = new Platform(650, 800, 0)
         this.add(platform)
-
-        let platform2 = new Platform(850, 800, 0)
-        this.add(platform2)
 
         let Kwal = new Enemy(900, 700)
         this.add(Kwal)
@@ -37,21 +28,30 @@ export class TestLevel extends ex.Scene{
 
         const platforms = [];
 
-        while (platforms.length < 100) {
-        const position = {
-        x: Math.floor(Math.random() * 500), 
-        y: Math.floor(Math.random() * 500), 
-        type: Math.floor(Math.random() * 2), 
-        };
-        platforms.push(position);
+        const numRows = 10;
+        const numCols = 10;
+        
+        for (let row = 0; row < numRows; row++) {
+          const curRow = [];
+          for (let col = 0; col < numCols; col++) {
+            const position = {
+              x: Math.floor(Math.random() * 500),
+              y: Math.floor(Math.random() * 500),
+              type: Math.floor(Math.random() * 2),
+            };
+            curRow.push(position);
+          }
+          platforms.push(curRow);
+          let emptyrow = [0, 0, 0, 0,0,0,0,0,0,0]
+          platforms.push(emptyrow)
         }
 
     
-        for(let platform in platforms)
-        {
-            let curplat = platforms[platform]
-            let newplatform = new Platform(curplat.x, curplat.y, curplat.type)
-            this.add(newplatform)
+     for (let row of platforms) {
+          for (let curplat of row) {
+            let newplatform = new Platform(curplat.x, curplat.y, curplat.type);
+            this.add(newplatform);
+          }
         }
 
         
