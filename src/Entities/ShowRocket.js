96f150fec0980bbb8fb23c898b493caeb5f823c9
RaymondLam18 <1024322@hr.nl>
2023-06-19 12:48:27 +0200

    update

--- src/Entities/ShowRocket.js
+++ src/Entities/ShowRocket.js
@@ -1,2 +1,6 @@
 import {Actor} from "excalibur";
 import {Resources} from "../resources.js";
+
+export class ShowRocket extends Actor {
+    
+}
--- src/Items/Rocket.js
+++ src/Items/Rocket.js
@@ -1,14 +1,17 @@
-import * as ex from 'excalibur'
 import {Item} from "../Core/Item.js";
 import {Resources} from "../resources.js";
+// import { ShowRocket } from "../Entities/ShowRocket.js";
 
 export class Rocket extends Item{
     constructor(x, y) {
-        super(x, y, Resources.Rocket);
+        super(x, y, Resources.Rocket, Engine);
     }
 
     //Only executes when a player collides.
     Action(player) {
-
+        player.setFlyRocket(true)
+        setTimeout(()=>{
+            player.setFlyRocket(false)
+        }, 1500)
     }
 }
