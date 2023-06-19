47d4c44abb044de33159f10f5e2c75e0fc26be7d
RaymondLam18 <1024322@hr.nl>
2023-06-19 13:29:15 +0200

    items update

--- src/Entities/ShowRocket.js
+++ src/Entities/ShowRocket.js
@@ -2,5 +2,13 @@ import {Actor} from "excalibur";
 import {Resources} from "../resources.js";
 
 export class ShowRocket extends Actor {
-    
+    constructor(player) {
+        super();
+        this.graphics.use(Resources.Rocket.toSprite())
+        this.player = player
+        this.pos = player.pos
+    }
+    onPreUpdate(_engine, _delta) {
+        this.pos = this.player.pos
+    }
 }
