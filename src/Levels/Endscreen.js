e45094cd536a91c28da8f01dcdcab7fd4f2806db
Atal09 <113996359+Atal09@users.noreply.github.com>
2023-06-21 11:10:26 +0200

    fixes + Endscreen.js
    fixes + Endscreen.js

---
+++ src/Levels/Endscreen.js
@@ -0,0 +1,25 @@
+import * as ex from 'excalibur'
+import {Player, PlayerName} from "../Entities/Player.js";
+import {Platform} from "../Core/Platform.js";
+import {Enemy} from "../Core/Enemy.js";
+import {Background} from "../Core/Area.js";
+
+import {Coin} from "../Items/Coin.js";
+import {Jetpack} from "../Items/Jetpack.js";
+import {Rocket} from "../Items/Rocket.js";
+import {Nuclear} from "../Items/Nuclear.js";
+import {Star} from "../Items/Star.js";
+
+
+
+export class Endscreen extends ex.Scene{
+
+    onPreUpdate(_engine, _delta) {
+        if (localStorage.getItem('Score')){
+            if (Math.ceil(this.points) > localStorage.getItem('Score')) {
+            localStorage.setItem('Score', Math.ceil(this.points));
+            }
+    
+        }
+    }
+}
