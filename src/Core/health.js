88eb8b3bee7923a60661b96960dd20b70635f5af
RaymondLam18 <1024322@hr.nl>
2023-06-22 11:04:18 +0200

    doesnt work yet

---
+++ src/Core/health.js
@@ -0,0 +1,31 @@
+import { Actor, SpriteSheet, Vector } from "excalibur";
+import { Resources } from "../resources";
+
+export class healthbar extends Actor {
+    spritesheet;
+    state;
+
+    constructor(curHealth) {
+        super();
+        this.state = curHealth;
+    }
+
+    onInitialize() {
+        this.spritesheet = SpriteSheet.fromImageSource({
+            image: Resources.Health,
+            grid: {
+                rows: 4,
+                columns: 1,
+                spriteWidth: 432,
+                spriteHeight: 144
+            }
+        });
+
+        this.pos = new Vector(700, 700);
+        this.graphics.use(this.spritesheet.getSprite(0, this.state));
+    }
+
+    onHealthUpdate(updatedHealth) {
+        this.graphics.use(this.spritesheet.getSprite(0, updatedHealth));
+    }
+}
